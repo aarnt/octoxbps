65e3f65633fb80860caa598e89bdc9a14e2808fd
Alexandre Arnt <falso@correio.com>
2015-10-12 10:00:51 -0300

    - Fixed void linux recognition code.

--- src/unixcommand.cpp
+++ src/unixcommand.cpp
@@ -1042,7 +1042,7 @@ LinuxDistro UnixCommand::getLinuxDistro()
 
       QString contents = file.readAll();
 
-      if (contents.contains(QRegExp("Void Linux")))
+      if (contents.contains("PRETTY_NAME=\"void\""))
       {
         ret = ectn_VOID;
       }
