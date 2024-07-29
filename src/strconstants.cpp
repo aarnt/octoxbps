a2eedb4656a81a2909f638cca9fa0548e3d9d129
Alexandre Arnt <qtgzmanager@gmail.com>
2024-07-29 10:58:38 -0300

    Version number bump

--- src/strconstants.cpp
+++ src/strconstants.cpp
@@ -28,7 +28,7 @@ QString StrConstants::getApplicationName(){
 }
 
 QString StrConstants::getApplicationVersion(){
-  return "0.3.4 (dev)";
+  return "0.3.4";
 }
 
 QString StrConstants::getQtVersion(){
